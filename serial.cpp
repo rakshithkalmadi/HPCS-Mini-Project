0620f50237ca92cf3c3af7d3dcf98054a3b15cca
SVB808 <84030242+SVB808@users.noreply.github.com>
2023-10-18 19:19:35 +0530

    Serial Code

---
+++ serial.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+int b(int a, int b, int c) {
+    int positive_count = 0;
+
+    if (a > 0) {
+        positive_count++;
+    }
+    if (b > 0) {
+        positive_count++;
+    }
+    if (c > 0) {
+        positive_count++;
+    }
+
+    return (positive_count >= 2) ? 1 : 0;
+}
+
+int main() {
+    Mat image = imread("image1.jpg", IMREAD_GRAYSCALE);
+
+    if (image.empty()) {
+        printf("Image not found or could not be opened.\n");
+        return 1;
+    }
+
+    Mat result_image(image.rows, image.cols, CV_8UC1); // Create a new image to store the results
+
+    for (int y = 0; y < image.rows; y++) {
+        for (int x = 0; x < image.cols; x++) {
+            int P0 = image.at<uchar>(y, x);
+            int P1 = (x < image.cols - 1) ? image.at<uchar>(y, x + 1) : 0;
+            int P2 = (x < image.cols - 1 && y < image.rows - 1) ? image.at<uchar>(y + 1, x + 1) : 0;
+            int P3 = (y < image.rows - 1) ? image.at<uchar>(y + 1, x) : 0;
+            int P4 = (x > 0 && y < image.rows - 1) ? image.at<uchar>(y + 1, x - 1) : 0;
+            int P5 = (x > 0) ? image.at<uchar>(y, x - 1) : 0;
+            int P6 = (x > 0 && y > 0) ? image.at<uchar>(y - 1, x - 1) : 0;
+            int P7 = (y > 0) ? image.at<uchar>(y - 1, x) : 0;
+            int C = P0; // Assuming center pixel is P0
+
+            int CP[8];
+            CP[0] = b(P7 - C, P0 - C, P1 - C);
+            CP[1] = b(P1 - C, P2 - C, P3 - C);
+            CP[2] = b(P3 - C, P4 - C, P5 - C);
+            CP[3] = b(P5 - C, P6 - C, P7 - C);
+            CP[4] = b(P6 - P0, C - P0, P2 - P0);
+            CP[5] = b(P4 - P2, C - P2, P0 - P2);
+            CP[6] = b(P2 - P4, C - P4, P6 - P4);
+            CP[7] = b(P0 - P6, C - P6, P4 - P6);
+
+            // Reverse the CP array and convert it to a decimal value
+            int decimal_value = 0;
+            for (int i = 7; i >= 0; i--) {
+                decimal_value = decimal_value * 2 + CP[i];
+            }
+
+            // Set the pixel value in the result image
+            result_image.at<uchar>(y, x)=static_cast<uchar>(decimal_value);
+        }
+    }
+
+    // Save the result image
+    imwrite("result_image.jpg", result_image);
+
+    return 0;
+}
+
